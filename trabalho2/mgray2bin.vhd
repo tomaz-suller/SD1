7480e5d394123ca7ee637e3665dbfd9fe8c54118
tomaz-suller <tomaz.suller@usp.br>
2020-09-23 09:16:19 -0300

    Define single gray2bin component

--- trabalho2/mgray2bin.vhd
+++ trabalho2/mgray2bin.vhd
@@ -1,3 +1,16 @@
+entity g is
+    port(
+        gin: in bit;
+        binp: in bit;
+        bout: out bit
+    );
+end entity;
+
+architecture gray of g is
+begin
+    bout <= binp xor gin;
+end architecture gray;
+
 entity gray2bin is
     generic (
         size: natural := 3
