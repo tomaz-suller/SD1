29aa383659aae3443b25234476368182e6ec65b1
tomaz-suller <tomaz.suller@usp.br>
2020-09-23 09:27:36 -0300

    Define main architecture using for-generate

--- trabalho2/mgray2bin.vhd
+++ trabalho2/mgray2bin.vhd
@@ -20,3 +20,33 @@ entity gray2bin is
         bin: out bit_vector(size-1 downto 0)
     );
 end entity;
+
+architecture iterative of gray2bin is
+
+    component g is
+        port(
+            gin: in bit;
+            binp: in bit;
+            bout: out bit
+        );
+    end component;
+
+    signal outp: bit;
+    signal temp: bit_vector(size-1 downto 0);
+
+begin
+    grays: for i in size-1 downto 0 generate
+    
+        g0: if i=(size-1) generate
+            gi: g port map(gray(i), '0', temp(i));
+        end generate;
+
+        general: if i<(size-1) generate
+            gi: g port map(gray(i), temp(i+1), temp(i));
+        end generate;
+
+    end generate grays;
+
+    bin <= temp;
+
+end architecture iterative;
