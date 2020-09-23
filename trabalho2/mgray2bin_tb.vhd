f9af1a0cbbc9a6173f0871ccd370e30179d2a92b
tomaz-suller <tomaz.suller@usp.br>
2020-09-23 09:15:16 -0300

    Make testbench for generic gray2bin

--- trabalho2/mgray2bin_tb.vhd
+++ trabalho2/mgray2bin_tb.vhd
@@ -13,6 +13,34 @@ architecture testbench of mgray2bin_tb is
         );
     end component;
     
-    signal g, b: bit_vector(2 downto 0);
+    signal g, b: bit_vector(3 downto 0);
 
 begin
+
+    dut: gray2bin
+        generic map(size => 4)
+        port map(gray=>g, bin=>b);
+    
+    tb_process: process
+    begin
+
+        report "BOT";
+
+        g <= "0011";
+        wait for 1 ns;
+        assert b = "0010" report "Teste 2 falhou!" severity warning;
+        
+        g <= "0101";
+        wait for 1 ns;
+        assert b = "0110" report "Teste 6 falhou!" severity warning;
+
+        g <= "1111";
+        wait for 1 ns;
+        assert b = "1010" report "Teste 10 falhou!" severity warning;
+
+        report "EOT";
+        wait;
+    end process;
+
+end testbench;
+
