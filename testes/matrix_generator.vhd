4e3dc68832afd93d6da74e5064ad7d4ab1628226
tomaz-suller <tomaz.suller@usp.br>
2020-10-25 09:27:18 -0300

    Fix index error

--- testes/matrix_generator.vhd
+++ testes/matrix_generator.vhd
@@ -1,8 +1,6 @@
 library ieee;
 use ieee.numeric_bit.all;
 use ieee.math_real.all;
-library work;
-use work.computation.all;
 
 package computation is
     function secded_message_size(data_size: positive) return integer;
@@ -17,14 +15,18 @@ end computation;
 
 --------------------------------------------
 
+library ieee;
+use ieee.numeric_bit.all;
+use ieee.math_real.all;
+use work.computation.all;
+
 entity matrix_generator is
     generic(
         data_size: positive := 4
     );
 
     port(
-        H: out bit_vector(104 downto 0)
-        -- H: out bit_vector( (secded_message_size(data_size)-1)*(secded_message_size(data_size)-1-data_size) - 1 downto 0) 
+        H: out bit_vector( (secded_message_size(data_size)-1)*(secded_message_size(data_size)-1-data_size) - 1 downto 0) 
         -- mem_data-1 * (mem_data-u_data-1)
     );
 end entity;
@@ -33,9 +35,9 @@ architecture arch of matrix_generator is
 
 begin
     -- data_size = 16 -> mem_data'length = 22 & u_data'length = 16
-    l: for i in 4 downto 0 generate -- (mem_data-u_data-2) downto 0
-        c: for j in 21 downto 1 generate -- mem_data-1 downto 1
-            H( 22*i+j ) <= '1' when to_integer(unsigned( ( bit_vector(to_unsigned(integer(2**real(i)), 5)) and bit_vector(to_unsigned(j, 5)) ) )) /= 0 else '0';
+    l: for i in (secded_message_size(data_size) - data_size - 2) downto 0 generate -- (mem_data-u_data-2) downto 0
+        c: for j in (secded_message_size(data_size)-1) downto 1 generate -- mem_data-1 downto 1
+            H( (secded_message_size(data_size)-1)*i+j-1 ) <= '1' when to_integer(unsigned( ( bit_vector(to_unsigned(integer(2**real(i)), integer(ceil(log2(real(secded_message_size(data_size))))))) and bit_vector(to_unsigned(j, integer(ceil(log2(real(secded_message_size(data_size))))))) ) )) /= 0 else '0';
             -- H( mem_data*i+j ) <= '1' when to_integer(unsigned( ( bit_vector(to_unsigned(integer(2**real(i)), integer(ceil(log2(real(mem_data))))) 
         end generate;
     end generate;
