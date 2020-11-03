c37bb4d597ac46cab388c157d221a18ba1bc66c1
tomaz-suller <tomaz.suller@usp.br>
2020-11-02 21:16:49 -0300

    Add functional fsm architecture

--- trabalho/5/zumbi.vhd
+++ trabalho/5/zumbi.vhd
@@ -1,3 +1,6 @@
+library ieee;
+use ieee.numeric_bit.rising_edge;
+
 entity zumbi is
     port(
         clock, reset: in bit;
@@ -26,3 +29,55 @@ begin
 
     z <= q0;
 end architecture;
+
+architecture fsm of zumbi is
+    
+    type states is (S, S2, AVp, AVi);
+    signal curr_st, next_st: states;
+    
+    function debug(cs: states) return string is
+    begin
+        if cs = S then
+            return "S";
+        elsif cs = S2 then
+            return "S2";
+        elsif cs = AVp then
+            return "AVp";
+        elsif cs = AVi then
+            return "AVi";
+        else
+            return "UNK";
+        end if;
+    end;
+
+begin
+
+    next_st <= 
+        S when x = "00"                     else
+        S when curr_st = S and x = "11"     else
+        S2 when x = "10"                    else
+        S2 when curr_st = S2 and x = "11"   else
+        AVp when x = "01"                   else
+        AVp when curr_st = AVi and x = "11" else
+        AVi when curr_st = AVp and x = "11" else
+        S;
+
+    action: process(clock, reset)
+    begin
+        if reset = '1' then
+            curr_st <= S;
+        elsif rising_edge(clock) then
+            curr_st <= next_st;
+            
+            report "Current state: " & debug(curr_st);
+            report "X: " & bit'image(x(1)) & bit'image(x(0));
+            report "Next state: " & debug(next_st);
+            report "";
+
+        end if;
+    end process;
+
+    z <= '1' when curr_st = S2 or curr_st = AVi else '0';
+
+end fsm;
+
