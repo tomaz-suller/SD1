997886c74776ee0f1bc83f4234173b5a945bb31b
tomaz-suller <tomaz.suller@usp.br>
2020-12-03 10:29:09 -0300

    Add test features with bv's

--- testes/bit_vector.vhd
+++ testes/bit_vector.vhd
@@ -1,11 +1,13 @@
 library ieee;
+use std.textio.all;
 use ieee.numeric_bit.all;
 
 entity bv is
     port(
         A, B: in bit_vector(3 downto 0);
         C: out bit_vector(3 downto 0);
-        D: out bit_vector(31 downto 0)
+        D: out bit_vector(31 downto 0);
+        E: out bit_vector(15 downto 0)
     );
 end entity;
 
@@ -20,7 +22,11 @@ architecture arch of bv is
         return tmp;
     end function;
 
+    constant hex: string := ;
+
 begin
+    hex <= "e48c";
     C <= A and B;
     D <= extend(A, '1');
+    E(15 downto 0) <= to_bitvector(hex);
 end arch;
--- testes/bit_vector_tb.vhd
+++ testes/bit_vector_tb.vhd
@@ -9,16 +9,18 @@ architecture arch of bv_tb is
         port(
             A, B: in bit_vector(3 downto 0);
             C: out bit_vector(3 downto 0);
-            D: out bit_vector(31 downto 0)
+            D: out bit_vector(31 downto 0);
+            E: out bit_vector(15 downto 0)
         );
     end component;
 
     signal A, B, C: bit_vector(3 downto 0);
     signal D: bit_vector(31 downto 0);
+    signal E: bit_vector(15 downto 0);
 
 begin
 
-    dut: bv port map(A, B, C, D);
+    dut: bv port map(A, B, C, D, E);
 
     tb: process
     begin
@@ -28,7 +30,8 @@ begin
         B <= "1010";
         wait for 5 ns;
 
-        report "extend: " & to_bstring(D) severity warning; 
+        report "extend: " & to_bstring(D) severity note; 
+        report "E: " & to_bstring(E) severity note;
 
         report "EOT";
         wait;
